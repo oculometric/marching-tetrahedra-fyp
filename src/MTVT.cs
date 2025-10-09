22d49ba238a8aef639f4a6a57bdb324e54c2852e
Jacob Costen <32058792+oculometric@users.noreply.github.com>
2025-10-09 16:08:34 +0100

    started the c# project

---
+++ src/MTVT.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+class MTVT
+{
+    public struct Mesh
+    {
+        List<Vector3> vertices;
+        List<Vector3> normals;
+        List<uint> indices;
+    }
+
+    private static void samplingPass(out float[] sample_points, Vector3 minimum_extent, Vector3 maximum_extent, float resolution)
+    {
+        // TODO: sampling pass - compute the values at all of the sample points
+    }
+
+    private static void flaggingPass(out int[] sample_prox_flags, in float[] sample_points, Vector3 grid_size)
+    {
+        // TODO: flagging pass - check all of the edges around each sample point, and set the edge flag bits
+    }
+
+    private static void vertexPass(out uint[] edge_indices, out List<Vector3> vertices, in float[] sample_points, in float[] sample_prox_flags)
+    {
+        // TODO: vertex pass - generate vertices for edges with flags set, and merge them where possible, assigning vertex references to these edges
+    }
+
+    private static void geometryPass(out List<uint> index_buffer, in uint[] edge_indices, in float[] sample_points, Vector3 grid_size)
+    {
+        // TODO: geometry pass - generate per-tetrahedron geometry from the edge/sample point info, discard triangles which zero size
+    }
+
+    // TODO: different lattice structures
+    // TODO: different merging techniques
+    // TODO: time/memory tracking
+    // TODO: vertex/index count
+    // TODO: summary generation tech
+
+    public static Mesh computeGeometry(Vector3 minimum_extent, Vector3 maximum_extent, float resolution, Func<Vector3, float> sample_func)
+    {
+        // TODO: root func
+        return new Mesh();
+    }
+}
---
+++ src/MainClass.cs
@@ -0,0 +1,10 @@
+﻿namespace mtvt
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
