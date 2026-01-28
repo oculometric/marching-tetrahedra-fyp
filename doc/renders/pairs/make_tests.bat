a96c8db7e81801e3cc00a0b16c9943e0b8d9fedd
Jacob Costen <32058792+oculometric@users.noreply.github.com>
2026-01-28 16:45:01 +0000

    redid the videos

--- doc/renders/pairs/make_tests.bat
+++ doc/renders/pairs/make_tests.bat
@@ -2,7 +2,6 @@
 @echo off
 echo let's get to work!
 
-set prefix=sf
 set first[0]=bu
 set secnd[0]=bi
 set first[1]=bu
@@ -22,11 +21,13 @@ set secnd[7]=si
 set first[8]=bp
 set secnd[8]=sp
 
-for %%n in (0,1,2,3,4,5,6,7,8) do (
-	set "input_a=!prefix!_!first[%%n]!"
-	set "input_b=!prefix!_!secnd[%%n]!"
-	set "output=!input_a!_!input_b!"
-	.\bin\ffmpeg.exe -i .\renders\!input_a!.gif -i .\renders\!input_b!.gif -filter_complex hstack -c:v libx264 -crf 10 !output!.mp4
+for %%p in (ss,sf,bs,as,af) do (
+	set prefix=%%p
+	for %%n in (0,1,2,3,4,5,6,7,8) do (
+		set "input_a=!prefix!_!first[%%n]!"
+		set "input_b=!prefix!_!secnd[%%n]!"
+		set "output=!input_a!_!input_b!"
+		.\bin\ffmpeg.exe -i .\renders\!input_a!.gif -i .\renders\!input_b!.gif -filter_complex "hstack,scale=-1:1024:flags=neighbor" -c:v libx264 -preset slow -crf 12 -profile:v high -pix_fmt yuv420p !output!.mp4
+	)
+	pause
 )
-
-pause
